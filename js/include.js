510d2cb8e6838dc4ab36154a15ff80e8ab9007af
Raphael Neuberger <70220250+RaphaelNeuberger@users.noreply.github.com>
2026-02-12 18:04:42 +0100

    footer and header

--- js/include.js
+++ js/include.js
@@ -2,17 +2,17 @@
 
 function includeHTML(selector, url) {
   fetch(url)
-    .then(response => response.text())
-    .then(data => {
+    .then((response) => response.text())
+    .then((data) => {
       document.querySelector(selector).innerHTML = data;
     });
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  if (document.querySelector('#header-include')) {
-    includeHTML('#header-include', 'header.html');
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector("#header-include")) {
+    includeHTML("#header-include", "header.html");
   }
-  if (document.querySelector('#footer-include')) {
-    includeHTML('#footer-include', 'footer.html');
+  if (document.querySelector("#footer-include")) {
+    includeHTML("#footer-include", "footer.html");
   }
 });
